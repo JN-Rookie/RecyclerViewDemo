75841f763a859ade632dfe103d797207dd3e42dc
JNfeicui-caiwei <wonner_cw@163.com>
2016-09-12 15:15:09 +0800

    Initial commit

---
+++ app/src/main/java/com/sjsm/recyclerviewdemo/DividerItemDecoration.java
@@ -0,0 +1,91 @@
+package com.sjsm.recyclerviewdemo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/9/12 0012.
+ */
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private static final int[] ATTRS           = new int[]{
+            android.R.attr.listDivider
+    };
+    public static final  int   HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
+
+    public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
+
+    private Drawable mDivider;
+
+    private int mOrientation;
+
+    public DividerItemDecoration(Context context, int orientation) {
+        final TypedArray a = context.obtainStyledAttributes(ATTRS);
+        mDivider = a.getDrawable(0);
+        a.recycle();
+        mOrientation = orientation;
+    }
+
+    public void setOrientation(int orientation) {
+        if (orientation != HORIZONTAL_LIST && orientation != VERTICAL_LIST) {
+            throw new IllegalArgumentException("invalid orientation");
+        }
+        mOrientation = orientation;
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        if (mOrientation == VERTICAL_LIST) {
+            drawVertical(c, parent);
+        } else {
+            drawHorizontal(c, parent);
+        }
+    }
+
+    private void drawVertical(Canvas c, RecyclerView parent) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+            RecyclerView v = new RecyclerView(parent.getContext());
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+    private void drawHorizontal(Canvas c, RecyclerView parent) {
+        int top = parent.getPaddingTop();
+        int bottom = parent.getHeight() - parent.getPaddingBottom();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+                    .getLayoutParams();
+            int left = child.getRight() + params.rightMargin;
+            int right = left + mDivider.getIntrinsicHeight();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        if(mOrientation==VERTICAL_LIST){
+            outRect.set(0,0,0,mDivider.getIntrinsicHeight());
+        }else {
+            outRect.set(0,0,mDivider.getIntrinsicWidth(),0);
+        }
+    }
+}
---
+++ app/src/main/java/com/sjsm/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,79 @@
+package com.sjsm.recyclerviewdemo;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView              mRecyclerView;
+    private List<Map<String, String>> mData;
+    private HomeAdapter               mAdapter;
+    private Context                   mContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext=this;
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        initData();
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.HORIZONTAL));
+        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(mContext));
+        mAdapter=new HomeAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void initData() {
+        mData = new ArrayList<>();
+        Map<String, String> map = null;
+        for (int i = 0; i < 100; i++) {
+            map = new HashMap<>();
+            map.put("text", "内容" + i);
+            mData.add(map);
+        }
+
+    }
+
+    private class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder=new MyViewHolder(LayoutInflater.from(mContext).inflate(R.layout.list_item,parent,false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv_Text.setText(mData.get(position).get("text"));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+         class MyViewHolder extends RecyclerView.ViewHolder{
+            TextView tv_Text;
+
+            public MyViewHolder(View view) {
+                super(view);
+                tv_Text= (TextView) view.findViewById(R.id.id_text);
+            }
+        }
+    }
+}
